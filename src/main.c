64de1f1a49c6951b961a663bcfd93f8515896ab4
shre0407 <shreyashwandre@gmail.com>
2025-05-14 13:18:56 +0530

    [any message]

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
 
@@ -11,5 +11,7 @@ int main(int argc, char *argv[]) {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+  input[strlen(input) - 1] = '\0';
+  printf("%s: command not found\n", input);
   return 0;
 }
